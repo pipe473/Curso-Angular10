c8a2d68be8514603016cdb6fe8ad9423bfbdd5ce
pipe473 <bedoyafelipe743@gmail.com>
2020-12-16 12:08:49 +0100

    haciendo pruebas para recibir informacion de un componente padre a un hijo

--- spa-avengers/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ spa-avengers/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe-tarjeta',
   templateUrl: './heroe-tarjeta.component.html'
 })
 export class HeroeTarjetaComponent implements OnInit {
+
+  heroe: any = {};
 
   constructor() { }
 
