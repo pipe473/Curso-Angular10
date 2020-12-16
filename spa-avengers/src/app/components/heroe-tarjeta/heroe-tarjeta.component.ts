159977be75de0ba76cee220266612ca1786c241a
pipe473 <bedoyafelipe743@gmail.com>
2020-12-16 18:25:39 +0100

    definiendo comunicacion del hijo hacia el padre

--- spa-avengers/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ spa-avengers/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,14 +10,16 @@ export class HeroeTarjetaComponent implements OnInit {
   @Input() heroe: any = {};
   @Input() index: number;
 
+  @Output() heroeSeleccionado: EventEmitter<number>
+
   constructor(private _router: Router) { }
 
   ngOnInit(): void {
   }
 
   verHeroe(){
-    console.log( this.index );
-    this._router.navigate( [ '/heroe', this.index ] );      
+    // console.log( this.index );
+    // this._router.navigate( [ '/heroe', this.index ] );      
   }
 
 }
