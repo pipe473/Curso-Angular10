a9006397d1224d0af9d90d1bd8ce78ea35e5b329
pipe473 <bedoyafelipe743@gmail.com>
2020-12-17 19:43:12 +0100

    pipes para monedas

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -7,10 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Capitán América';
-
   arreglo = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-
   personajes: string[] = ['Ironman', ' Spiderman', ' Thor', ' Loki', ' Groot', ' Superman'];
-
   PI: number = 3.141592653589793;
+  porcentaje:number = 0.234;
+  salario:number = 1234.5;
 }
