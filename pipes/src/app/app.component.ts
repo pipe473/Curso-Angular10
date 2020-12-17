56d1d62592f5b3dc1a532f2282b96374c8adfc0c
pipe473 <bedoyafelipe743@gmail.com>
2020-12-17 20:02:12 +0100

    pipe para json

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -7,9 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Capitán América';
-  arreglo = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
   personajes: string[] = ['Ironman', ' Spiderman', ' Thor', ' Loki', ' Groot', ' Superman'];
   PI: number = 3.141592653589793;
   porcentaje:number = 0.234;
   salario:number = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'primera',
+      casa: 20
+    }
+  }
 }
