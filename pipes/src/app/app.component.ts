c77df18e70c0e3477985fc4ef151c6de6f2017e0
pipe473 <bedoyafelipe743@gmail.com>
2020-12-17 19:29:15 +0100

    Pipes de numeros decimales

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -9,4 +9,8 @@ export class AppComponent {
   nombre: string = 'Capitán América';
 
   arreglo = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+  personajes: string[] = ['Ironman', ' Spiderman', ' Thor', ' Loki', ' Groot', ' Superman'];
+
+  PI: number = 3.141592653589793;
 }
