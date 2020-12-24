0ea078864c5bc868deb43f625e9fc3a56cf54449
pipe473 <bedoyafelipe743@gmail.com>
2020-12-24 16:49:50 +0100

    creando un pipe para añadir videos con angular

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -14,7 +14,9 @@ export class AppComponent {
   porcentaje:number = 0.234;
   salario:number = 1234.5;
   fecha: Date = new Date();
+
   idioma: string = 'en';
+  videoUrl: string = 'https://www.youtube.com/embed/hpBBvzrtr60';
 
   CambioIdioma(){
     
