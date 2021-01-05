500bec43c9d56507ac79f4748306f052d061ec6d
pipe473 <bedoyafelipe743@gmail.com>
2021-01-05 13:03:00 +0100

    problema de los headers resuelto y la llamada a la api funcionando

--- spotiapp/src/app/components/home/home.component.ts
+++ spotiapp/src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
--- spotiapp/src/app/services/spotify.service.ts
+++ spotiapp/src/app/services/spotify.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +12,11 @@ export class SpotifyService {
 
   getNewReleases(){
 
-    this.http.get('	https://api.spotify.com/v1/browse/new-releases').subscribe( data => {
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQCpYsJ5tkSHcbwp_4-IBpIuTiQg2P5wo3ToZkRpqZ-T0Y2N5Pq-4Ird1NIW5NxFdZNTSgFsqJa2W4q1yIU'
+    });
+
+    this.http.get('	https://api.spotify.com/v1/browse/new-releases?limit=20', { headers }).subscribe( data => {
       console.log(data);      
     });
   }
