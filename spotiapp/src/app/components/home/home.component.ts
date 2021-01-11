0702bf643c26b897ef3b39b469c22741a660c3e2
pipe473 <bedoyafelipe743@gmail.com>
2021-01-11 20:29:44 +0100

    Manejo de errores de un observable

--- spotiapp/src/app/components/home/home.component.ts
+++ spotiapp/src/app/components/home/home.component.ts
@@ -11,14 +11,23 @@ export class HomeComponent implements OnInit {
   nuevasCanciones: any[] = [];
   loading: boolean;
 
+  error: boolean;
+  mensajeError: string;
+
   constructor( private spotify: SpotifyService  ) { 
 
     this.loading = true;
+    this.error = false;
 
     this.spotify.getNewReleases()
       .subscribe( (data: any) => {
         this.nuevasCanciones = data;
         this.loading = false;
+    }, ( errorServicio ) => {
+      this.loading = false;
+      this.error = true;
+      console.log(errorServicio);
+      this.mensajeError = errorServicio.error.error.message;
     });
   }
 
--- spotiapp/src/app/services/spotify.service.ts
+++ spotiapp/src/app/services/spotify.service.ts
@@ -15,7 +15,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/${ query }`;
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQDHvy6u3xtLbF7yZQP7HJEc0vhHhD_Jqa9__cBW8-xnB-kXOcj12ST0O_CVaA4pWcUxvBo1wBsUf1Gk7JA'
+      'Authorization': 'Bearer BQDHvy6u3xtLbF7yZQP7HJEc0vhHhD_Jqa9__cBW8-xnB-kXOcj12ST0O_CVaA4pWcUxvBo1wBsUf1Gk7J'
     });
 
     return this.http.get(url, { headers });
