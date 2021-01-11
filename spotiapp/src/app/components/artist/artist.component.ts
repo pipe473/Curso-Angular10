c99b939efa91813054f2e9311224dfb5e78116bb
pipe473 <bedoyafelipe743@gmail.com>
2021-01-11 19:10:02 +0100

    obteniendo artista desde spotify

--- spotiapp/src/app/components/artist/artist.component.ts
+++ spotiapp/src/app/components/artist/artist.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-artist',
   templateUrl: './artist.component.html',
-  styleUrls: ['./artist.component.css']
+  styleUrls: ['./artist.component.css'],
 })
 export class ArtistComponent {
+  artista: any = {};
+  loadingArtist: boolean;
 
-  constructor(private router: ActivatedRoute) { 
-
-    this.router.params.subscribe( params => {
-      console.log(params['id']);      
+  constructor(private router: ActivatedRoute, private spotify: SpotifyService) {
+    this.loadingArtist = true;
+    this.router.params.subscribe((params) => {
+      this.getArtista(params['id']);
     });
   }
 
+  // Metodo que genera el artista
+  getArtista(id: string) {
+    this.loadingArtist = true;
+    this.spotify.getArtista(id).subscribe((artista) => {
+      console.log(artista);
+      this.artista = artista;
+      this.loadingArtist = false;
+    });
+  }
 }
--- spotiapp/src/app/services/spotify.service.ts
+++ spotiapp/src/app/services/spotify.service.ts
@@ -16,7 +16,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/${ query }`;
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCq6jSxiOJkyoMOzt7Vzeq-y3TYTfqT2DLXrzMGxAwGDcQykvEEw-XEKm7JdUv-Pfv0YniUjxYtOetmYs0'
+      'Authorization': 'Bearer BQBCO9aH3MFBlMA_rXJud4jawN19OoZKdhebHj6axjPxBp5_ORKhsLO71nJQLz_1KXXkCRU_s55_Zcv8apI'
     });
 
     return this.http.get(url, { headers });
@@ -27,8 +27,13 @@ export class SpotifyService {
           .pipe( map ( data => data['albums'].items ));      
   }
 
-  getArtista( termino: string ) {
+  getArtistas( termino: string ) {
     return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
           .pipe( map (data =>  data['artists'].items ));      
+  }
+
+  getArtista( id: string ) {
+    return this.getQuery(`artists/${ id }`)
+          // .pipe( map (data =>  data['artists'].items ));      
   }
 }
