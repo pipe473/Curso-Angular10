0e92a97fd9cd20e9edf516937bf003e5cd71e09b
pipe473 <bedoyafelipe743@gmail.com>
2021-01-16 14:58:18 +0100

    separando las rutas hijas a un archivo en especifico

--- ciclo-vida-componentes/src/app/app-routing.module.ts
+++ ciclo-vida-componentes/src/app/app-routing.module.ts
@@ -3,24 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 
-
-// Rutas hijas
-
-import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
-import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
-import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { CHILD_ROUTES } from './components/usuario/usuario.routes';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { 
     path:'usuario/:id',
     component: UsuarioComponent,
-    children: [
-      { path: 'nuevo', component: UsuarioNuevoComponent },
-      { path: 'editar', component: UsuarioEditarComponent },
-      { path: 'detalle', component: UsuarioDetalleComponent },
-      { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
-    ]
+    children: CHILD_ROUTES
   },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
