06ea6b94f5c65a1b7dd51d6f0a7421b2a001f43f
pipe473 <bedoyafelipe743@gmail.com>
2021-01-16 14:22:15 +0100

    añadiendo rutas para hacer pruebas

--- ciclo-vida-componentes/src/app/app-routing.module.ts
+++ ciclo-vida-componentes/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path:'usuario', component: UsuarioComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
