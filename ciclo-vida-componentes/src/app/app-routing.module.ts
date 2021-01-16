01abdc1776c141a938e00ef7c3927e7768bfe796
pipe473 <bedoyafelipe743@gmail.com>
2021-01-16 14:46:26 +0100

    creando rutas hijas

--- ciclo-vida-componentes/src/app/app-routing.module.ts
+++ ciclo-vida-componentes/src/app/app-routing.module.ts
@@ -3,9 +3,25 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 
+
+// Rutas hijas
+
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path:'usuario', component: UsuarioComponent },
+  { 
+    path:'usuario/:id',
+    component: UsuarioComponent,
+    children: [
+      { path: 'nuevo', component: UsuarioNuevoComponent },
+      { path: 'editar', component: UsuarioEditarComponent },
+      { path: 'detalle', component: UsuarioDetalleComponent },
+      { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
+    ]
+  },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
