b22592730bf7a7c87d7214570b9594e45300599d
pipe473 <bedoyafelipe743@gmail.com>
2021-01-19 19:15:01 +0100

    obteniendo el perfil del usuario que inició sesión

--- loginAuth0/src/app/app.component.ts
+++ loginAuth0/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
 
 @Component({
@@ -6,10 +6,12 @@ import { AuthService } from './services/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-  constructor(private auth: AuthService){
-    
+  constructor(private auth: AuthService){ }
+
+  ngOnInit(): void {
+    this.auth.localAuthSetup();
   }
 
 }
