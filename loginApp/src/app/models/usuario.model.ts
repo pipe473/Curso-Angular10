bb7690d84f59525a1b0270a9ded07edb4aeab350
pipe473 <bedoyafelipe743@gmail.com>
2021-01-20 19:07:18 +0100

    conectar el formulario de registro con una instancia del modelo de usuario

--- loginApp/src/app/models/usuario.model.ts
+++ loginApp/src/app/models/usuario.model.ts
@@ -3,3 +3,4 @@ export class UsuarioModel {
     password: string;
     nombre: string;
 }
+
--- loginApp/src/app/pages/registro/registro.component.ts
+++ loginApp/src/app/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-registro',
@@ -7,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  usuario: UsuarioModel;
+
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.usuario = new UsuarioModel();
 
+    this.usuario.email = 'bedoyafelipe743@gmail.com';
+  }
+
+  onSubmit() {
+    console.log('Formulario enviado');
+    console.log(this.usuario);    
+  }
 
 }
