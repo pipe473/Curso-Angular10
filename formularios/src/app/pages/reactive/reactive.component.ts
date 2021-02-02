ab6cabe1e2bd7d0cfb6054734d79be1e48b054af
pipe473 <bedoyafelipe743@gmail.com>
2021-02-02 19:47:05 +0100

    Validaciones reactivas en el html

--- formularios/src/app/pages/reactive/reactive.component.ts
+++ formularios/src/app/pages/reactive/reactive.component.ts
@@ -21,6 +21,18 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get nombreNoValido(){
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+
+  get apellidoNoValido(){
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  }
+
+  get correoNoValido(){
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  }
+
   crearFormulario(){
     this.forma = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(5)] ],
@@ -30,7 +42,15 @@ export class ReactiveComponent implements OnInit {
   }
 
   guardar(){
-    console.log( this.forma );    
+    console.log( this.forma );   
+    
+    if ( this.forma.invalid ) {
+
+      return Object.values( this.forma.controls ).forEach( control => {
+        control.markAsTouched();     
+      });
+      
+    }
   }
 
 }
