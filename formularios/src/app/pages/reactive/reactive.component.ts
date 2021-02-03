f3e8e738a0d49bbfe24b2b871120db4715951757
pipe473 <bedoyafelipe743@gmail.com>
2021-02-03 17:09:07 +0100

    Añadiendo validacion visual de los campos anidados

--- formularios/src/app/pages/reactive/reactive.component.ts
+++ formularios/src/app/pages/reactive/reactive.component.ts
@@ -29,6 +29,14 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('correo').invalid && this.forma.get('correo').touched;
   }
 
+  get distritoNoValido() {
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched;
+  }
+
+  get ciudadNoValida() {
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
+  }
+
   crearFormulario() {
     this.forma = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(5)]],
@@ -45,8 +53,13 @@ export class ReactiveComponent implements OnInit {
     console.log(this.forma);
 
     if (this.forma.invalid) {
-      return Object.values(this.forma.controls).forEach((control) => {
-        control.markAsTouched();
+      return Object.values( this.forma.controls ).forEach( control  => {
+
+        if ( control instanceof FormGroup) {
+          Object.values( control.controls ).forEach( control => control.markAllAsTouched() );
+        } else {
+          control.markAllAsTouched();
+        }     
       });
     }
   }
