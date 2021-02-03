474283e5e6ce71cff6655189055fc8cf2ce3a72d
pipe473 <bedoyafelipe743@gmail.com>
2021-02-03 16:51:35 +0100

    agrupaciones de los objetos con formGroupName para recoger los datos

--- formularios/src/app/pages/reactive/reactive.component.ts
+++ formularios/src/app/pages/reactive/reactive.component.ts
@@ -4,53 +4,50 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
-  styleUrls: ['./reactive.component.css']
+  styleUrls: ['./reactive.component.css'],
 })
 export class ReactiveComponent implements OnInit {
-
-
   forma: FormGroup;
 
-
-  constructor( private fb: FormBuilder ) { 
-
+  constructor(private fb: FormBuilder) {
     this.crearFormulario();
-
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  get nombreNoValido(){
+  get nombreNoValido() {
     return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
   }
 
-  get apellidoNoValido(){
-    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  get apellidoNoValido() {
+    return (
+      this.forma.get('apellido').invalid && this.forma.get('apellido').touched
+    );
   }
 
-  get correoNoValido(){
+  get correoNoValido() {
     return this.forma.get('correo').invalid && this.forma.get('correo').touched;
   }
 
-  crearFormulario(){
+  crearFormulario() {
     this.forma = this.fb.group({
-      nombre: ['', [Validators.required, Validators.minLength(5)] ],
+      nombre: ['', [Validators.required, Validators.minLength(5)]],
       apellido: ['', Validators.required],
-      correo: ['', [ Validators.required ,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+      correo: ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$') ] ],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required ],
+        ciudad: ['', Validators.required ]
+      })
     });
   }
 
-  guardar(){
-    console.log( this.forma );   
-    
-    if ( this.forma.invalid ) {
+  guardar() {
+    console.log(this.forma);
 
-      return Object.values( this.forma.controls ).forEach( control => {
-        control.markAsTouched();     
+    if (this.forma.invalid) {
+      return Object.values(this.forma.controls).forEach((control) => {
+        control.markAsTouched();
       });
-      
     }
   }
-
 }
