faaaa5b996b1f1b374db541d40615d04245b581b
pipe473 <bedoyafelipe743@gmail.com>
2021-02-05 18:33:08 +0100

    detectando cambios en los valores de mi formulario

--- formularios/src/app/pages/reactive/reactive.component.ts
+++ formularios/src/app/pages/reactive/reactive.component.ts
@@ -16,6 +16,8 @@ export class ReactiveComponent implements OnInit {
     this.crearFormulario();
     this.cargarDataAlFormulario();
 
+    this.crearListeners();
+
   }
 
   ngOnInit(): void {}
@@ -77,6 +79,17 @@ export class ReactiveComponent implements OnInit {
     }, {
       validators: this.validadores.passwordsIguales('password1', 'password2')
     });
+  }
+
+  crearListeners(){
+    // this.forma.valueChanges.subscribe( valor => {
+    //   console.log(valor);     
+    // });
+
+    // this.forma.statusChanges.subscribe( status => console.log({ status }));
+
+    this.forma.get('nombre').valueChanges.subscribe(console.log);
+
   }
 
   cargarDataAlFormulario(){
